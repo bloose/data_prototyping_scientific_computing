918bccffaa7f36061588bcf7759d0d8ac4a379b1
bloose <bloose@uri.edu>
2023-09-07 13:33:48 -0400

    new semester

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,8 +3,8 @@
 from __future__ import unicode_literals
 
 AUTHOR = u'Brice Loose'
-SITENAME = u'2021 | Data Prototyping and Scientific Computing'
-SUBTITLE = 'Fall 2021'
+SITENAME = u'2023 | Data Prototyping and Scientific Computing'
+SUBTITLE = 'Fall 2023'
 SITEURL = 'https://bloose.github.io/data_prototyping_scientific_computing'
 
 RELATIVE_URLS = True
