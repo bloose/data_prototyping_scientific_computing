cb86c70b62c64b884a5e3395f7c3ae759acab531
bloose <bloose@uri.edu>
2023-10-29 20:39:44 -0400

    ./bashpush593.sh

---
+++ output/Assignments/Therm_Lab_2023.ino
@@ -0,0 +1,44 @@
+
+// This is the declaration section.  Assign pin numbers to variables for use in code.
+
+// This line assigns Analog Pin 0 to the variable aPin.
+
+
+
+
+//  This is setup function.  It initializes the pins as well as the serial communication between
+//  Arduino and the computer.
+void setup() {
+  // Tell the Serial monitor to use a baud rate of 9600.  This is the transfer rate.
+
+
+  
+}
+
+// This is the loop structure that will operate continuously as long as the arduino is powered.
+void loop() {
+  
+  
+  // Use the pinMode function to tell Arduino to put Analog Pin 0 into INPUT mode so it can watch for
+  // a signal.
+
+
+
+  //Read an integer from analogRead and record
+
+
+  // Make the analog to digital conversion to report the value in Volts.  See Data Measurement notes.
+  
+  
+  // Do the same for Analog Pin 2.
+
+
+
+  // Compute the resistance as indicated in the lab instructions and print to the Serial port.
+  
+
+  // Add a delay so it doesn't go too fast to read.
+
+
+
+}
