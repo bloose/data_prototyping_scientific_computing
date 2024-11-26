4f51584dd79c4a1ae181bd00b64f58fc5bb7f931
bloose <bloose@uri.edu>
2024-11-26 14:07:38 -0500

    ./bashpush593.sh

--- output/Assignments/coreclock.py
+++ output/Assignments/coreclock.py
@@ -10,6 +10,7 @@ Core clock to confirm parallel processing.
 
 if __name__ == '__main__':
 
+    # Load these two functions from dask.distributed
 	from dask.distributed import Client, LocalCluster
 	
 	from dask.distributed import progress
@@ -20,19 +21,22 @@ if __name__ == '__main__':
 
 
 
-	# WRITE coreclock() module:
-	# Take input, x.
-	# Do operation on x (this is optional).
-	# Sleep for 1 second.
-	# Return x or f(x).
-
+	# WRITE coreclock() module that does the following:
+	# 1. Take input, x.
+	# 2. Do operation on x (this is optional).
+	# 3. Sleep for 1 second. (This is the key line for testing that parallel processing is working.
+	# 4. Return x or f(x).
 
 	
-	# client.map(module,list) will distribute the operations of module on list, amongst
-	# the available processes. Modify the inputs to client.map() to perform the coreclock()
-	# module on 500 values of x.    
+	# Use the funciton client.map(module,list_of_x) to distribute the operations of 'module' on 'list_of_x', amongst
+	# the available cpus. Modify the inputs to client.map() to perform the coreclock()
+	# module on a list of 500 values of x.    
 	# f = client.map(module,list_of_500_x)
 
 	
 	# Display and update the processor progress.
 	progress(f)
+
+    # Don't forget to include code to measure the start time and end time for this entire script to run.
+    # Use that start and end time to compute a dt and compare with the time you expected it to run if using
+    # only one cpu.  For further instruction, refer back to HPC_Parallel_Processing.ipynb.
